d4cd4e49da720b274549aad4fa85b72efd792f4d
PrejithPrasobhanan <prejithprasobhanan@gmail.com>
2023-01-15 14:58:27 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:weatherx/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home : Home(),
+    routes: {
+
+    },
+  ));
+}
+
